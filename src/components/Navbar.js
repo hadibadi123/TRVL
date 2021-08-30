156267a051ba77b1d14986d4f891cfd0137cf3e2
Falak Nabelsi <hadistudi275@gmail.com>
2021-08-30 12:04:49 -0400

    initial push

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react' 
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+ 
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+    return (
+    <>
+      <nav classname="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Hadi 
+            <i class="fas fa-users"></i>
+          </Link>  
+          <div className='menu-icon' onClick={handleClick }>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='-/' className='nav-links' onClick={closeMobileMenu}>
+                home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services' className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+           {Button && <Button ButtonStyle='btn--outline'>SIGN UP</Button>}
+        </div>  
+      </nav>
+    </>
+    );
+}
+
+export default Navbar;
